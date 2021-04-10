af44eea4f1f467c7957ea2c882d4f28924303d81
Jose Juarez <juarez.junior@outlook.com>
2021-04-09 21:20:34 -0300

    Adding project Metodos

---
+++ Metodo/Metodos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Metodo
+{
+    class Metodos
+    {
+        #region Métodos com parâmetros
+        public void Cumprimentar()
+        {
+            Console.WriteLine("Seja bem vindo!");
+        }
+        public void Cumprimentar(string nome)
+        {
+            Console.WriteLine($"Seja bem vindo {nome}!");
+        }
+        #endregion
+    }
+}
---
+++ Metodo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos metodo = new Metodos();
+            metodo.Cumprimentar();
+            metodo.Cumprimentar("Juarez");
+        }
+    }
+}
